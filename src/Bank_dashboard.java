e6cf62764d9fbcbb674f14323998c7b2416922c8
Ramzan <Ramzanismailovr@gmail.com>
2024-06-17 18:54:42 +0200

    First commit

--- src/Bank_dashboard.java
+++ src/Bank_dashboard.java
@@ -1,23 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Bank_dashboard {
 
 	public static void showDashboard(int id, String fullName) {
-	
+		int option = 0;
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		
+		
 		while(true) {
+				
+		System.out.println("Welcome to Bank of Chechnya\r" + fullName);
+		
+		System.out.println("Your options");
+		System.out.println("1. Deposit");	
+		System.out.println("2. Withdraw");
+		System.out.println("3. Send cash");
+		System.out.println("4. Logout");
+		
+			try {
+				
+				option = Integer.parseInt(br.readLine());
+				
+				
+				switch(option) {
+				
+				
+				case 1: 
+					Process.depositCash(id);
+					break;
+				
+				case 2: 
+					
+					break; 
+					
+				case 3: 
+					
+					break; 
+				
+				case 4: 
+					
+					break;
+				
+				default: 
+					System.out.println("Invalid option, try again");
+					break;
+				
+					}
+				
+				} catch (Exception e) {
 			
-			
-			System.out.println("Welcome " + fullName);
-			
-			
-			
-			
+			}	
 		}
-		
-		
-		
-	}
-	
-	
+	}	
 }
--- src/Process.java
+++ src/Process.java
@@ -1,11 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Process {
  
 	static BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 	static int id;
-	static String firstName, lastName, password;
+	static String firstName, lastName, password, sql;
+	static Connection con = Db_conf.getConnection();
 
 	public static void createAccount() { //Processing create account from main
 		
@@ -50,8 +55,8 @@ public class Process {
 			
 			
 			if (Accounts.loginAccount(id, password)){
-                System.out.println(
-                    "Login Successful\n");
+				
+					
             } else {
                 System.out.println(
                     "Failed!\n");
@@ -62,5 +67,123 @@ public class Process {
             	e.printStackTrace();
         }					
 	}
+	
+	
+	public static void depositCash(int id) {
+		
+		double amount = 0;					
+		
+		while(true) {
+		try {
+			System.out.println("Current balance: " + getCurrentBalance(id));
+			System.out.println("Amount to deposit:");
+			amount = Double.parseDouble(br.readLine());
+			
+			if(amount <= 0 ) {
+				
+				System.out.println("Amount has to be greater than 0");
+				continue;
+					
+			}
+			
+			con.setAutoCommit(false); //Start transaction
+		   
+			double currentBalance = getCurrentBalance(id);
+			if(currentBalance == -1) {
+				
+				System.out.println("Account not found");
+				return;
+				
+			}
+			
+			double newBalance = currentBalance + amount;
+			if(updateBalance(id, newBalance)) {
+				
+				System.out.println("New balance is: " + newBalance);
+				con.commit();
+				
+			} else {
+				
+				System.out.println("An error occurred, try again");
+				con.rollback();
+				
+			}
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			try {
+                con.rollback(); // Rollback transaction on error
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }	finally {
+            try {
+                con.setAutoCommit(true); // Reset auto-commit to true
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }		
+	}	
+	
+}
+		
+private static double getCurrentBalance(int id){
+		
+		sql = "SELECT balance FROM customer WHERE c_id = ?";
+		
+		try(PreparedStatement ps = con.prepareStatement(sql)) {
+			
+			ps.setInt(1, id);
+			
+			try(ResultSet rs = ps.executeQuery()){
+				
+				if(rs.next()) {	
+					return rs.getDouble("balance");	
+				}
+			
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+
+		} catch(SQLException e) {
+			
+			e.printStackTrace();
+			
+		}		
+		return -1;	
+}
+			
+
+	private static boolean updateBalance(int id, double balance){
+		
+		sql = "UPDATE customer SET balance = ? WHERE c_id = ?";
+		
+		try(PreparedStatement ps = con.prepareStatement(sql)) {
+			
+			ps.setDouble(1, balance);
+			ps.setInt(2, id);
+			
+			int rows = ps.executeUpdate();
+			
+			return rows > 0;
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+				
+		}
+		
+
+		return false;
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 }
